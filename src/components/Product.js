c78e686c55a0ece816d428de0f12afad8a59351b
Salvador Vilca <salvasvip@gmail.com>
2022-01-17 21:24:47 -0400

    events

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,10 +2,14 @@ import {Fragment} from "react";
 
 const Product = ({product}) => {
     const {name, price, id} = product
+    const selectProduct = id => {
+        console.log(`buying product ${id}`)
+    }
     return (
         <Fragment>
             <h2>{name}</h2>
             <p>${price}</p>
+            <button type="button" onClick={()=>selectProduct(id)}>Comprar</button>
         </Fragment>
     )
 }
